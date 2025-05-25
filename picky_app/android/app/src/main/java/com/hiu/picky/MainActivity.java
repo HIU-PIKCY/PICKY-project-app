ae73ffab6c298ca5fada2aa23e82e1b416284a3f
오채민 <ocmin20022@gmail.com>
2025-05-26 04:05:26 +0900

    Feat: CRA + capacitor 세팅

---
+++ picky_app/android/app/src/main/java/com/hiu/picky/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hiu.picky;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ picky_app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hiu.picky',
+  appName: 'picky',
+  webDir: 'build'
+};
+
+export default config;
